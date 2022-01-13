6678fd3b97a8bfbe0f8c5db8c4dc2b085aca1779
Carolina <cindy.s.r.28@hotmail.com>
2022-01-13 18:31:25 -0500

    add styles to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-// import "./styles.css";
+import "./styles.css";
 
 const Header = () => {
   return (
     <nav className="nav">
       <ul className="nav__list">
-        <li className="nav__list-item">Contacto</li>
+        <li className="nav__list-item nav__list-item-contact">Contacto</li>
         <li className="nav__list-item">Home</li>
         <li className="nav__list-item">Precios</li>
         <li className="nav__list-item">Blog</li>
