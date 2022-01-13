6ad958060cf7c75c86566e7c8d690481a8be48ee
Carolina <cindy.s.r.28@hotmail.com>
2022-01-13 12:47:17 -0500

    create header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+// import "./styles.css";
+
+const Header = () => {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__list-item">Contacto</li>
+        <li className="nav__list-item">Home</li>
+        <li className="nav__list-item">Precios</li>
+        <li className="nav__list-item">Blog</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
