0c73765ee2e6ce4a34761c57932cdaf6bff1fa6a
Carolina <cindy.s.r.28@hotmail.com>
2022-01-13 19:12:27 -0500

    create footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
 import Header from "../src/components/Header/Header";
 import Login from "./components/Login/Login";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
       <Login />
+      <Footer />
     </div>
   );
 }
